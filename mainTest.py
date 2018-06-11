aa17b8e02f5956b0c647d3431a7c4a3b6f57b08d
yuanxiaokun <yuanxiaokun@outlook.com>
2018-06-11 23:36:21 +0800

    函数测试

--- mainTest.py
+++ mainTest.py
@@ -1,4 +1,5 @@
 import os
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import tensorflow as tf
@@ -7,3 +8,10 @@ hello = tf.constant("hello world")
 sess = tf.Session()
 
 print(sess.run(hello))
+
+
+def add(param, param1):
+    return param + param1
+
+
+print(add(16, 87))
