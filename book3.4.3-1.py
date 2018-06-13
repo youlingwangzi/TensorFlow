8b66546cb633e90ac4145e09370a8b78bd893ea1
yuanxiaokun <youlingwangzi1995@qq.com>
2018-06-14 00:44:58 +0800

    3.4.3-神经网络参数与TensorFlow变量

---
+++ book3.4.3-1.py
@@ -0,0 +1,24 @@
+# page55
+import tensorflow as tf
+
+w1 = tf.Variable(tf.random_normal((2, 3), stddev=1, seed=1))
+w2 = tf.Variable(tf.random_normal((3, 1), stddev=1, seed=1))
+
+x = tf.constant([[0.7, 0.9]])
+
+a = tf.matmul(x, w1)
+y = tf.matmul(a, w2)
+
+with tf.Session() as sess:
+    sess.run(w1.initializer)
+    sess.run(w2.initializer)
+
+    print(sess.run(y))
+
+# 使用初始化函数来初始化变量
+
+with tf.Session() as sess:
+    init_op = tf.global_variables_initializer()
+    sess.run(init_op)
+
+    print(sess.run(y))
--- chapter3/tenserflow_test.py
+++ chapter3/tenserflow_test.py
@@ -8,6 +8,7 @@ result = a + b
 # Page 40
 print(result)
 
+###################################################################
 # Page 41
 print(a.graph is tf.get_default_graph())
 
@@ -30,3 +31,15 @@ with tf.Session(graph=g2) as sess:
     tf.global_variables_initializer().run()
     with tf.variable_scope("", reuse=True):
         print(sess.run(tf.get_variable("V")))
+
+print("Page54 ================================================")
+# Page 54
+
+#声明一个矩阵变量2*3
+weights = tf.Variable(tf.random_normal([2, 3], stddev=2))
+
+#生成一个偏置项变量，初始值为0，长度为3
+biases = tf.Variable(tf.zeros([3]))
+
+
+
