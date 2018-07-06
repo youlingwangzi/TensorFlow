954d04f556dca57daeaaecf0958da85642ed2eb4
yuanxiaokun <yuanxiaokun@outlook.com>
2018-07-07 01:07:31 +0800

    5.2.1 Tensorflow 训练神经网络-完整的训练程序

---
+++ chapter5/book5.2.1-1.py
@@ -0,0 +1,119 @@
+# 神经网络模型训练及不同模型对比结果
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+INPUT_NODE = 784  # 输入节点
+OUTPUT_NODE = 10  # 输出节点
+LAYER1_NODE = 500  # 隐藏层数
+
+BATCH_SIZE = 100  # 每次batch打包的样本个数
+
+# 模型相关的参数
+LEARNING_RATE_BASE = 0.8  # 基础学习率
+LEARNING_RATE_DECAY = 0.99  # 学习率是衰减率
+REGULARAZTION_RATE = 0.0001  # 描述模型复杂度的正则化项在损失函数中的系数
+TRAINING_STEPS = 5000  # 训练轮数
+MOVING_AVERAGE_DECAY = 0.99  # 滑动平均衰减率
+
+
+# 定义的辅助函数，给定神经网络的输入和所有参数，计算神经网络的前向传播结果。
+# 这里是一个三层的全连接神经网络。输入层、一个隐藏层、输出层共三层。
+def inference(input_tensor, avg_class, weights1, biases1, weights2, biases2):
+    # 不使用滑动平均类
+    if avg_class is None:
+        # 计算隐藏层的前向传播结果，使用ReLU激活函数。
+        layer1 = tf.nn.relu(tf.matmul(input_tensor, weights1) + biases1)
+        # 计算输出层的前向传播结果
+        return tf.matmul(layer1, weights2) + biases2
+
+    else:
+        # 使用滑动平均类
+        # 首先使用avg_class.average函数计算出变量的滑动平均值，然后参与前向传播计算
+        layer1 = tf.nn.relu(tf.matmul(input_tensor, avg_class.average(weights1)) + avg_class.average(biases1))
+        return tf.matmul(layer1, avg_class.average(weights2)) + avg_class.average(biases2)
+
+
+def train(mnist):
+    x = tf.placeholder(tf.float32, [None, INPUT_NODE], name='x-input')
+    y_ = tf.placeholder(tf.float32, [None, OUTPUT_NODE], name='y-input')
+    # 生成隐藏层的参数。
+    weights1 = tf.Variable(tf.truncated_normal([INPUT_NODE, LAYER1_NODE], stddev=0.1))
+    biases1 = tf.Variable(tf.constant(0.1, shape=[LAYER1_NODE]))
+    # 生成输出层的参数。
+    weights2 = tf.Variable(tf.truncated_normal([LAYER1_NODE, OUTPUT_NODE], stddev=0.1))
+    biases2 = tf.Variable(tf.constant(0.1, shape=[OUTPUT_NODE]))
+
+    # 计算不含滑动平均类的前向传播结果
+    y = inference(x, None, weights1, biases1, weights2, biases2)
+
+    # 定义训练轮数及相关的滑动平均类
+    global_step = tf.Variable(0, trainable=False)
+    variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
+    variables_averages_op = variable_averages.apply(tf.trainable_variables())
+    average_y = inference(x, variable_averages, weights1, biases1, weights2, biases2)
+
+    # 计算交叉熵及其平均值
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y_, 1))
+    cross_entropy_mean = tf.reduce_mean(cross_entropy)
+
+    # 损失函数的计算
+    regularizer = tf.contrib.layers.l2_regularizer(REGULARAZTION_RATE)
+    regularaztion = regularizer(weights1) + regularizer(weights2)
+    loss = cross_entropy_mean + regularaztion
+
+    # 设置指数衰减的学习率。
+    learning_rate = tf.train.exponential_decay(
+        LEARNING_RATE_BASE,
+        global_step,
+        mnist.train.num_examples / BATCH_SIZE,
+        LEARNING_RATE_DECAY,
+        staircase=True)
+    # 优化损失函数
+    train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+
+    # 反向传播更新参数和更新每一个参数的滑动平均值
+    with tf.control_dependencies([train_step, variables_averages_op]):
+        train_op = tf.no_op(name='train')
+
+    # 计算正确率
+    correct_prediction = tf.equal(tf.argmax(average_y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    # 初始化会话，并开始训练过程。
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+        validate_feed = {x: mnist.validation.images, y_: mnist.validation.labels}
+        test_feed = {x: mnist.test.images, y_: mnist.test.labels}
+
+        # 循环的训练神经网络。
+        for i in range(TRAINING_STEPS):
+            if i % 1000 == 0:
+                validate_acc = sess.run(accuracy, feed_dict=validate_feed)
+                print("After %d training step(s), validation accuracy using average model is %g " % (i, validate_acc))
+
+            xs, ys = mnist.train.next_batch(BATCH_SIZE)
+            sess.run(train_op, feed_dict={x: xs, y_: ys})
+
+        test_acc = sess.run(accuracy, feed_dict=test_feed)
+        print(("After %d training step(s), test accuracy using average model is %g" % (TRAINING_STEPS, test_acc)))
+
+
+def main(argv=None):
+    mnist = input_data.read_data_sets("../datasets/MNIST_data", one_hot=True)
+    train(mnist)
+
+
+if __name__ == '__main__':
+    main()
+
+'''
+output
+After 0 training step(s), validation accuracy using average model is 0.0734 
+After 1000 training step(s), validation accuracy using average model is 0.9792 
+After 2000 training step(s), validation accuracy using average model is 0.9822 
+After 3000 training step(s), validation accuracy using average model is 0.9834 
+After 4000 training step(s), validation accuracy using average model is 0.9838 
+After 5000 training step(s), test accuracy using average model is 0.9819
+'''
+
